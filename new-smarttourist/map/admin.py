e3016b479938d51ba1470e7577b88c865dc50fad
Ranjan Shrestha <074bex435.ranjan@pcampus.edu.np>
2022-02-28 18:19:57 +0545

    added genre model with populated data

--- new-smarttourist/map/admin.py
+++ new-smarttourist/map/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Place
+from .models import Place,Genre
 from django.contrib.gis import admin
 
 # Register your models here.
 admin.site.register(Place)
+admin.site.register(Genre)
