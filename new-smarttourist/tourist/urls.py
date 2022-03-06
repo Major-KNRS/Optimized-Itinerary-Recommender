833f4357297f2465f7637c6c0a015eb5d9e30b64
Ranjan Shrestha <074bex435.ranjan@pcampus.edu.np>
2022-03-06 09:43:13 +0545

    added functionality of returning user_id and email along with token in drf login

--- new-smarttourist/tourist/urls.py
+++ new-smarttourist/tourist/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .apiviews import UserCreate, UserUpdate
+from .apiviews import UserCreate, UserUpdate, CustomAuthToken
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path("", UserCreate.as_view(), name="user_create"),
-    path("login",views.obtain_auth_token,name="login"),
+    path("login",CustomAuthToken.as_view(),name="login"),
     path("<int:pk>/", UserUpdate.as_view(), name="user_update"),
 ]
